d740eb541760c7d56f1dd93578ff1a945ba2007f
Studio Colibri <bonjour@studiocolibri.be>
2018-11-16 20:36:44 +0100

    added favicons + updated color pickr + canvas icon close

--- assets/js/canvas.js
+++ assets/js/canvas.js
@@ -134,11 +134,21 @@ function showCanvas() {
     }
 }
 
-var canvasBtn = document.getElementById("canvas-button");
-canvasBtn.addEventListener('click', drawCanvas);
+function replaceBtnImg() {
+    var canvasBtnImg = document.getElementById("canvas-button__img");
+    if (canvasBtnImg.classList.contains('open')) {
+        canvasBtn.innerHTML = '<svg id="canvas-button__img" width="40px" height="40px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40"><path d="M11.8 29.1h-.3c-.4-.2-.6-.4-.6-.9l1-6.3c0-.4.1-.6.4-.9l9.9-9.8c.4-.4.8-.4 1.2 0l5.4 5.4.4.6v.3c-.1.1-.1.3-.3.4L18.9 28c-.1.1-.3.2-.5.2l-.6.1-4.7.7-1.3.1zm9.8-13.4l-.9-.9-6.8 6.9.9.9 6.8-6.9zm2.8 2.9l-6.9 6.8.9.9 6.9-6.8-.9-.9zm-7.9 5.7l6.9-6.8-.7-.7-6.9 6.8.7.7zm6.3-11.6l-1 1 4.6 4.7 1-1.1-4.6-4.6zm-5.7 14.2L13.2 23v.1l-.3 2.4.1.2 1.5 1.5.2.1 1.6-.3.8-.1z" fill="#fff"/><path fill="none" stroke="#fff" stroke-width="2" stroke-miterlimit="10" d="M10.9 11l18.2 18.1"/></svg>';
+    } else {
+        canvasBtn.innerHTML = '<svg id="canvas-button__img" class="open" width="40px" height="40px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1080 1080"><path d="M319 787h-8c-12-5-17-12-15-23l26-171c1-10 4-17 11-24l266-265c11-11 21-11 32 0l145 145 11 15v8c-2 4-4 8-7 10L509 754c-4 4-8 6-14 6l-15 3-126 18-35 6zm264-363l-25-25-184 185 25 24 184-184zm77 77L475 685l24 24 185-184-24-24zM446 656l185-184-20-20-185 184 20 20zm169-313l-28 28 125 126 28-29-125-125zM461 727L356 622l-1 3-9 64 2 5 41 41 6 2 43-7 23-3z" fill="#fff"/></svg>';
+    }
+}
 
 function drawCanvas() {
     windowSize();
     initCanvas();
     showCanvas();
+    replaceBtnImg();
 }
+
+var canvasBtn = document.getElementById("canvas-button");
+canvasBtn.addEventListener('click', drawCanvas);
