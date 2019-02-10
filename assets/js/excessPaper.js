66112f4b417292d5a3934ace2a8aee0ba520a731
Studio Colibri <bonjour@studiocolibri.be>
2019-02-10 22:46:44 +0100

    changeColor cmsable + netlify cms color config

--- assets/js/excessPaper.js
+++ assets/js/excessPaper.js
@@ -1,6 +1,8 @@
 var excessPaper = document.getElementById('excess-paper');
 var excessPaperText = document.getElementById('excess-paper-text');
 
-excessPaper.addEventListener('click', function() {
-    excessPaperText.classList.toggle('visible');
-});
+if (excessPaper) {
+    excessPaper.addEventListener('click', function() {
+        excessPaperText.classList.toggle('visible');
+    });
+}
